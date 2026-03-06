d2ce92ae5a8e8abf1d379f9a19e091a55f6f131e
Dhvanitmonpara <monparadhvanit@gmail.com>
2026-03-06 20:33:39 +0530

    style: logo fix

--- web/src/components/general/Logo.tsx
+++ web/src/components/general/Logo.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Logo() {
   return (
     <Link href="/" className='mb-3 w-full'>
-      <h1 className="font-avallon p-4 pt-6 pb-8 text-3xl sm:text-4xl w-fit text-zinc-400 hover:text-zinc-100 transition-colors text-center select-none">Flick</h1>
+      <h1 className="font-avallon p-4 pt-6 pb-8 text-3xl sm:text-4xl w-fit text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-zinc-100 transition-colors text-center select-none">Flick</h1>
     </Link>
   )
 }
