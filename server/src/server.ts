5d8aa83b9bac833e7be1c904c706444b2c9a4657
Dhvanitmonpara <monparadhvanit@gmail.com>
2026-03-07 20:03:45 +0530

    fix: domain port issue

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,12 @@ import createApp from "@/app";
 import { env } from "@/config/env";
 
 const port = env.PORT;
+const host = env.NODE_ENV === "development" ? "localhost" : "0.0.0.0";
 
 const main = async () => {
 	const app = createApp();
-	const serverInstance = app.listen(port, () => {
-		console.log(`Server is listening on port ${port}`);
+	const serverInstance = app.listen(port, host, () => {
+		console.log(`Server is listening on ${host}:${port}`);
 	});
 
 	serverInstance.on("error", (error) => {
